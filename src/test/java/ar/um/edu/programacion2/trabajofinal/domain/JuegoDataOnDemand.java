d9dda9e27c9080984d4366f5fc76a9c9db1ae5f4
Gaston <g.suarezduek@gmail.com>
2016-11-01 20:11:05 -0300

    Primer up, sin mucho configuración

---
+++ src/test/java/ar/um/edu/programacion2/trabajofinal/domain/JuegoDataOnDemand.java
@@ -0,0 +1,6 @@
+package ar.um.edu.programacion2.trabajofinal.domain;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Juego.class)
+public class JuegoDataOnDemand {
+}
---
+++ src/test/java/ar/um/edu/programacion2/trabajofinal/domain/JuegoIntegrationTest.java
@@ -0,0 +1,11 @@
+package ar.um.edu.programacion2.trabajofinal.domain;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Juego.class)
+public class JuegoIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
